ef982a4aa4e1d7a68e6076e605ef20279153cfa6
Jessy Damasceno <jessy_bas@hotmail.com>
2022-09-09 20:37:23 -0300

    feat: add find product by ID model and tests

--- tests/unit/models/products.model.test.js
+++ tests/unit/models/products.model.test.js
@@ -7,7 +7,7 @@ const productsModel = require("../../../src/models/products.model");
 const { products } = require("./mocks/products.model.mock");
 
 describe("Testes de unidade do model de Produtos", function () {
-  describe("Lista todos os produtos", function () {
+  describe("Listando todos os produtos", function () {
     before(async function () {
       sinon.stub(connection, "execute").resolves([products]);
     });
@@ -24,6 +24,22 @@ describe("Testes de unidade do model de Produtos", function () {
         { id: 2, name: "Traje de encolhimento" },
         { id: 3, name: "Escudo do Capitão América" },
       ]);
+    });
+  });
+
+  describe("Listando um produto", function () {
+    before(async function () {
+      sinon.stub(connection, "execute").resolves([[products[0]]]);
+    });
+
+    after(async function () {
+      connection.execute.restore();
+    });
+
+    it("Testando o retorno da função findById", async function () {
+      const result = await productsModel.findById(1);
+
+      expect(result).to.be.deep.equal({ id: 1, name: "Martelo de Thor" });
     });
   });
 });
