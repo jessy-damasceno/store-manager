7636e0a5b7a86c0e73332961edbe4cc2ee5c6c50
Jessy Damasceno <jessy_bas@hotmail.com>
2022-09-09 18:24:30 -0300

    feat: add products model unit test

---
+++ tests/unit/models/products.model.test.js
@@ -0,0 +1,29 @@
+const { expect } = require("chai");
+const sinon = require("sinon");
+
+const connection = require("../../../src/models/connection");
+const productsModel = require("../../../src/models/products.model");
+
+const { products } = require("./mocks/products.model.mock");
+
+describe("Testes de unidade do model de Produtos", function () {
+  describe("Lista todos os produtos", function () {
+    before(async function () {
+      sinon.stub(connection, "execute").resolves([products]);
+    });
+
+    after(async function () {
+      connection.execute.restore();
+    });
+
+    it("Testando o retorno da função findAll", async function () {
+      const result = await productsModel.findAll();
+
+      expect(result).to.be.deep.equal([
+        { id: 1, name: "Martelo de Thor" },
+        { id: 2, name: "Traje de encolhimento" },
+        { id: 3, name: "Escudo do Capitão América" },
+      ]);
+    });
+  });
+});
