c6144b5d7ec0306783297cc48e8f22db5a3b9244
Jessy Damasceno <jessy_bas@hotmail.com>
2022-09-09 18:26:28 -0300

    fix: connection

--- src/models/connection.js
+++ src/models/connection.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || 33060,
+  port: process.env.MYSQL_PORT || 3306,
   user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'root',
+  password: process.env.MYSQL_PASSWORD || 'password',
   database: 'StoreManager',
 });
 
