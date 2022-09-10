4a1002b0c8607a03fcf2c5aed1b66c03e0767a7d
Jessy Damasceno <jessy_bas@hotmail.com>
2022-09-10 15:12:52 -0300

    chore[product.service]: create createProduct function

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -1,4 +1,5 @@
 const productsModel = require('../models/products.model');
+const validations = require('./validations/validations');
 
 async function listAll() {
   const productsList = await productsModel.findAll();
@@ -15,7 +16,23 @@ async function getProductById(productId) {
   return { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
 }
 
+async function createProduct(productName) {
+  const error = validations.validateNewProduct(productName);
+
+  if (error.type) {
+    return error;
+  }
+
+  const insertId = await productsModel.insert(productName);
+
+  return {
+    type: null,
+    message: { id: insertId, name: productName },
+  };
+}
+
 module.exports = {
   listAll,
   getProductById,
+  createProduct,
 };
