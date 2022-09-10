886d559c424fe5e019198e8b8140d768aa6a14bb
Jessy Damasceno <jessy_bas@hotmail.com>
2022-09-10 16:13:21 -0300

    fix[schemas]: addProductSchema string()

--- src/services/validations/schemas.js
+++ src/services/validations/schemas.js
@@ -1,7 +1,7 @@
 const Joi = require('joi');
 
 const addProductSchema = Joi.object({
-  name: Joi.string.min(5).required().messages({
+  name: Joi.string().min(5).required().messages({
     'string.empty': '"name" is required',
     'string.min': '"name" length must be at least 5 characters long',
   }),
