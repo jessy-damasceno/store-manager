05562fbb9995349e0da2f2cd2a3da53d63f70ec5
Jessy Damasceno <jessy_bas@hotmail.com>
2022-09-10 18:11:41 -0300

    feat[products.controller]: createProduct function

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -17,7 +17,19 @@ const getOneProduct = async (req, res) => {
   return res.status(200).json(message);
 };
 
+const createProduct = async (req, res) => {
+  const { name } = req.body;
+  const { type, message } = await productsService.createProduct(name);
+
+  if (type) {
+    return res.status(mapError(type)).json({ message });
+  }
+
+  return res.status(201).json(message);
+};
+
 module.exports = {
   listAllProducts,
   getOneProduct,
+  createProduct,
 };
