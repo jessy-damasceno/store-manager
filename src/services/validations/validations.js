50ead15bcef7e24497c398f34ee1c495addfe4d6
Jessy Damasceno <jessy_bas@hotmail.com>
2022-09-11 16:46:13 -0300

    chore: add validateNewSale validation

--- src/services/validations/validations.js
+++ src/services/validations/validations.js
@@ -1,4 +1,4 @@
-const { addProductSchema } = require('./schemas');
+const { addProductSchema, newSaleSchema } = require('./schemas');
 
 const validateNewProduct = (name) => {
   const { error } = addProductSchema.validate({ name });
@@ -12,6 +12,19 @@ const validateNewProduct = (name) => {
   return { type: null };
 };
 
+const validateNewSale = (payload) => {
+  const { error } = newSaleSchema.validate(payload);
+
+  if (error) {
+    return {
+      type: error.details[0].type === 'number.greater' ? 'INVALID_FIELD' : 'FIELD_REQUIRED',
+      message: error.details[0].message,
+    };
+  }
+  return { type: null };
+};
+
 module.exports = {
   validateNewProduct,
+  validateNewSale,
 };
