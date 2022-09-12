06c279252da6a821e4bc4d05ee8637dcdb6731d7
Jessy Damasceno <jessy_bas@hotmail.com>
2022-09-12 01:47:42 -0300

    chore: add sales service test productId is required case 2

--- tests/unit/services/sales.service.test.js
+++ tests/unit/services/sales.service.test.js
@@ -35,8 +35,15 @@ describe("Verificando service Sales", function () {
       expect(result).to.deep.equal(newSaleResponse);
     });
 
-    it.only("Retorna um erro se productId não for passado", async function () {
+    it("Retorna um erro se productId não for passado", async function () {
       const result = await salesService.createSale(missingProductId);
+      console.log(result);
+      expect(result instanceof Object).to.equal(true);
+      expect(result).to.deep.equal(PRODUCT_ID_REQUIRED_ERROR_MESSAGE);
+    });
+
+    it.only("Retorna um erro se productId não for passado - case 2", async function () {
+      const result = await salesService.createSale(missingProductId2);
       console.log(result);
       expect(result instanceof Object).to.equal(true);
       expect(result).to.deep.equal(PRODUCT_ID_REQUIRED_ERROR_MESSAGE);
