9a65eda5c2ab0abb8f6e41bcaa55c9af7f4b0316
Jessy Damasceno <jessy_bas@hotmail.com>
2022-09-14 01:10:37 -0300

    feat: updateProduct controller and route /PUT /products/:id

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -28,8 +28,22 @@ const createProduct = async (req, res) => {
   return res.status(201).json(message);
 };
 
+const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const { type, message } = await productsService.updateProduct({ id, name });
+
+    if (type) {
+      return res.status(mapError(type)).json({ message });
+    }
+
+    return res.status(200).json(message);
+};
+
 module.exports = {
   listAllProducts,
   getOneProduct,
   createProduct,
+  updateProduct,
 };
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -3,14 +3,15 @@ const {
   listAllProducts,
   getOneProduct,
   createProduct,
+  updateProduct,
 } = require('../controllers/products.controller');
 
 const productsRouter = Router();
 
 productsRouter.get('/:id', getOneProduct);
+productsRouter.put('/:id', updateProduct);
 
 productsRouter.get('/', listAllProducts);
-
 productsRouter.post('/', createProduct);
 
 module.exports = productsRouter;
